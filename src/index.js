67ecf6f9922e512a8709fbe49e0cd54a11aa08e9
Alfredo Rafael Pabon <alfredrafael@gmail.com>
2020-07-20 16:58:41 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'; // adds Bootstrap
+import { findDOMNode } from 'react-dom' //adds jquery
+import $ from 'jquery'; // adds jquery
+
+
+
+
+ReactDOM.render(
+  <React.Fragment>
+    <App />
+  </React.Fragment>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
